3ce3cf072f75c1d85d7639d58f7e6cb35ae48268
magdaly.santos <magdaly1201@gmail.com>
2022-10-28 01:22:52 -0300

    add cache and ttl 30 minutes

--- src/main/java/com/magdy/challenge/tenpo/core/percentage/port/PercentageClient.java
+++ src/main/java/com/magdy/challenge/tenpo/core/percentage/port/PercentageClient.java
@@ -1,10 +1,13 @@
 package com.magdy.challenge.tenpo.core.percentage.port;
 
+import org.springframework.cache.annotation.Cacheable;
+
 import java.io.IOException;
 import java.util.Optional;
 
 public interface PercentageClient {
 
+    @Cacheable(value = "PERCENTAGE")
     Optional<Integer> getPercentage() throws IOException;
 
 }
---
+++ src/main/java/com/magdy/challenge/tenpo/infrastructure/app/config/CacheConfig.java
@@ -0,0 +1,34 @@
+package com.magdy.challenge.tenpo.infrastructure.app.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+
+@Configuration
+@EnableCaching
+@EnableScheduling
+public class CacheConfig {
+
+    public static final String PERCENTAGE = "PERCENTAGE";
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager(PERCENTAGE);
+        return cacheManager;
+    }
+
+    @CacheEvict(allEntries = true, value = {PERCENTAGE})
+    @Scheduled(fixedDelay = 30 * 60 * 1000, initialDelay = 1 * 60 * 1000)
+    public void reportCacheEvict() {
+        System.out.println("Flush Cache " + LocalDate.now());
+    }
+}
