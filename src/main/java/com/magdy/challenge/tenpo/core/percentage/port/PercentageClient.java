f1a0b6dc3c9ed9fbb8397f0d8173588af608f140
magdaly.santos <magdaly1201@gmail.com>
2022-10-31 19:32:02 -0300

    change cacheable

--- src/main/java/com/magdy/challenge/tenpo/core/percentage/port/PercentageClient.java
+++ src/main/java/com/magdy/challenge/tenpo/core/percentage/port/PercentageClient.java
@@ -1,12 +1,9 @@
 package com.magdy.challenge.tenpo.core.percentage.port;
 
-import org.springframework.cache.annotation.Cacheable;
-
 import java.util.Optional;
 
 public interface PercentageClient {
 
-    @Cacheable(value = "PERCENTAGE")
     Optional<Integer> getPercentage();
 
 }
