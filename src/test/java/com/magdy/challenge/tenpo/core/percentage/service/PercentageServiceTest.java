339bb2a8f5776d0e040ea60b26abfad614bf0aab
magdaly.santos <magdaly1201@gmail.com>
2022-11-01 20:18:25 -0300

    add test

---
+++ src/test/java/com/magdy/challenge/tenpo/core/percentage/service/PercentageServiceTest.java
@@ -0,0 +1,43 @@
+package com.magdy.challenge.tenpo.core.percentage.service;
+
+import com.magdy.challenge.tenpo.core.history.service.HistoryService;
+import com.magdy.challenge.tenpo.core.percentage.port.PercentageClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PercentageServiceTest {
+
+    @InjectMocks
+    private PercentageService percentageService;
+
+    @Mock
+    private HistoryService historyService;
+
+    @Mock
+    private PercentageClient percentageClient;
+
+    @Test
+    @DisplayName("obtener porcentaje")
+    void percentage() {
+        Integer percentage = 10;
+
+        when(percentageClient.getPercentage()).thenReturn(Optional.of(percentage));
+
+        float results = percentageService.obtainPercentage();
+
+        assertEquals(10, results);
+        verify(percentageClient).getPercentage();
+    }
+
+}
---
+++ src/test/java/com/magdy/challenge/tenpo/core/sum/SumServiceTest.java
@@ -0,0 +1,38 @@
+package com.magdy.challenge.tenpo.core.sum;
+
+import com.magdy.challenge.tenpo.core.percentage.service.PercentageService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SumServiceTest {
+
+    @InjectMocks
+    private SumService sumService;
+
+    @Mock
+    private PercentageService percentageService;
+
+    @Test
+    @DisplayName("operacion de Suma de valores y porcentaje")
+    void operationSumValuesAndPercentage() {
+        int value1 = 5;
+        int value2 = 5;
+
+        when(percentageService.obtainPercentage()).thenReturn(10F);
+
+        float results = sumService.operationSumValuesAndPercentage(value1, value2);
+
+        assertEquals(11, results);
+        assertNotEquals(2, results);
+    }
+
+}
