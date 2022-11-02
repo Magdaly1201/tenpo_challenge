ed091ece84cfa80d40d89d5fe5755ba228673e8a
magdaly.santos <magdaly1201@gmail.com>
2022-11-01 22:41:48 -0300

    add test

---
+++ src/test/java/com/magdy/challenge/tenpo/core/sum/service/HistoryServiceTest.java
@@ -0,0 +1,45 @@
+package com.magdy.challenge.tenpo.core.sum.service;
+
+import com.magdy.challenge.tenpo.core.sum.port.HistoryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class HistoryServiceTest {
+
+    @InjectMocks
+    private HistoryService historyService;
+
+    @Mock
+    private HistoryRepository historyRepository;
+
+    private final LocalDateTime dummyTime = LocalDateTime.now();
+
+    @Test
+    @DisplayName("Crear transaccion historica")
+    void createTransaction(){
+
+        String type = "OK";
+        String userRequest ="userRequestTest";
+        String payload="payloadTest";
+        String status = "statusTest";
+
+        doNothing().when(historyRepository).createTransaction(any());
+
+        historyService.createTransaction(type,userRequest,payload,status);
+
+        verify(historyRepository).createTransaction(any());
+
+    }
+
+}
