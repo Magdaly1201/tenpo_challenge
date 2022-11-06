eaff4d0a62bafea383174e0e66a2a366dd30cfca
magdaly.santos <magdaly1201@gmail.com>
2022-11-06 20:57:08 -0300

    add const static

--- src/main/java/com/magdy/challenge/tenpo/adapter/repository/HistoryAdapterRepository.java
+++ src/main/java/com/magdy/challenge/tenpo/adapter/repository/HistoryAdapterRepository.java
@@ -1,6 +1,8 @@
 package com.magdy.challenge.tenpo.adapter.repository;
 
 import com.magdy.challenge.tenpo.core.history.model.History;
+import com.magdy.challenge.tenpo.core.history.model.Status;
+import com.magdy.challenge.tenpo.core.history.model.TypeTransaction;
 import com.magdy.challenge.tenpo.core.history.port.HistoryRepository;
 import com.magdy.challenge.tenpo.infrastructure.repository.dao.HistoryDao;
 import com.magdy.challenge.tenpo.infrastructure.repository.entity.HistoryEntity;
@@ -25,7 +27,7 @@ public class HistoryAdapterRepository implements HistoryRepository {
 
     @Override
     public float getLastPercentage() {
-        String percentage = historyDao.findFirstByStatusAndTypeOrderByDateTimeDesc("OK", "percentage").orElseThrow(() -> new RuntimeException("error obteniendo percentage")).getPayload();
+        String percentage = historyDao.findFirstByStatusAndTypeOrderByDateTimeDesc(Status.OK, TypeTransaction.PERCENTAGE).orElseThrow(() -> new RuntimeException("error obteniendo percentage")).getPayload();
         return Float.parseFloat(percentage);
     }
 
