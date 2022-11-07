5f15293f0b459f89331ce03a1dea5e77bf18593b
magdaly.santos <magdaly1201@gmail.com>
2022-11-07 12:46:11 -0300

    add test

--- src/test/java/com/magdy/challenge/tenpo/core/percentage/service/PercentageServiceTest.java
+++ src/test/java/com/magdy/challenge/tenpo/core/percentage/service/PercentageServiceTest.java
@@ -1,6 +1,9 @@
 package com.magdy.challenge.tenpo.core.percentage.service;
 
+import com.magdy.challenge.tenpo.core.history.model.Status;
+import com.magdy.challenge.tenpo.core.history.model.TypeTransaction;
 import com.magdy.challenge.tenpo.core.history.service.HistoryService;
+import com.magdy.challenge.tenpo.core.message.service.MessageService;
 import com.magdy.challenge.tenpo.core.percentage.port.PercentageClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -25,6 +32,9 @@ class PercentageServiceTest {
     private HistoryService historyService;
 
     @Mock
+    private MessageService messageService;
+
+    @Mock
     private PercentageClient percentageClient;
 
     @Test
@@ -33,11 +43,11 @@ class PercentageServiceTest {
         Integer percentage = 10;
         Long userId = 10L;
         when(percentageClient.getPercentage()).thenReturn(Optional.of(percentage));
+        doNothing().when(messageService).createMessage(TypeTransaction.PERCENTAGE, userId, percentage.toString(), Status.OK);
 
         float results = percentageService.obtainPercentage(userId);
 
         assertEquals(10, results);
         verify(percentageClient).getPercentage();
     }
-
 }
--- src/test/java/com/magdy/challenge/tenpo/core/sum/SumServiceTest.java
+++ src/test/java/com/magdy/challenge/tenpo/core/sum/SumServiceTest.java
@@ -1,6 +1,9 @@
 package com.magdy.challenge.tenpo.core.sum;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.magdy.challenge.tenpo.core.history.model.Status;
+import com.magdy.challenge.tenpo.core.history.model.TypeTransaction;
+import com.magdy.challenge.tenpo.core.message.service.MessageService;
 import com.magdy.challenge.tenpo.core.percentage.service.PercentageService;
 import com.magdy.challenge.tenpo.core.sum.model.Sum;
 import com.magdy.challenge.tenpo.core.sum.service.SumService;
@@ -13,6 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,6 +28,9 @@ class SumServiceTest {
     private SumService sumService;
 
     @Mock
+    private MessageService messageService;
+
+    @Mock
     private PercentageService percentageService;
 
     @Test
@@ -29,13 +38,26 @@ class SumServiceTest {
     void operationSumValuesAndPercentage() throws JsonProcessingException {
         int value1 = 5;
         int value2 = 5;
-        Long userId = 10L;
+        Long userId = 30L;
+        float percentage = 10;
 
+        String  payload = "{\n" +
+                "  \"value1\" : 5,\n" +
+                "  \"value2\" : 5,\n" +
+                "  \"sum\" : 10,\n" +
+                "  \"percentageSum\" : 1.0,\n" +
+                "  \"percentageValue\" : 10.0,\n" +
+                "  \"total\" : 11.0\n" +
+                "}";
         when(percentageService.obtainPercentage(userId)).thenReturn(10F);
+        doNothing().when(messageService).createMessage(anyString(),anyLong(),anyString(),anyString());
 
         Sum results = sumService.operationSumValuesAndPercentage(value1, value2, userId);
 
-        assertEquals(11, results.getSum());
+        assertEquals(10, results.getSum());
+        assertEquals(percentage, results.getPercentageValue());
+        assertEquals(11, results.getTotal());
+
         assertNotEquals(2, results.getPercentageSum());
     }
 }
